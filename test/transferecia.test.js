e4be6da23326f4d033fe9f6d12a18e1c95019eb6
Narayane <narayaneluvizutto@gmail.com>
2026-04-28 14:23:43 -0300

    Adicionando a biblioteca .env para variáveis globais

--- test/transferecia.test.js
+++ test/transferecia.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require('dotenv').config()
 
 describe ('Transferências', () => {
     describe ('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
             //Capturar o token
-            const respostaLogin = await request ('http://localhost:3000')
+            const respostaLogin = await request (process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type','application/json')
                  .send({
@@ -15,7 +16,7 @@ describe ('Transferências', () => {
             //Resultado da autenticação
             const token = respostaLogin.body.token  
             
-            const resposta = await request ('http://localhost:3000')
+            const resposta = await request (process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type','application/json')
                 .set('Authorization', `Bearer ${token}`)
@@ -29,7 +30,7 @@ describe ('Transferências', () => {
         })
         it('Deve retornar falha com 422 quando o valor da transferencia for menor que R$ 10,00', async () => {
             //Capturar o token
-            const respostaLogin = await request ('http://localhost:3000')
+            const respostaLogin = await request (process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type','application/json')
                  .send({
@@ -39,7 +40,7 @@ describe ('Transferências', () => {
             //Resultado da autenticação
             const token = respostaLogin.body.token  
             
-            const resposta = await request ('http://localhost:3000')
+            const resposta = await request (process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type','application/json')
                 .set('Authorization', `Bearer ${token}`)
